c8887d4c644f016927fdca24d9db2a918d186729
ZOLotishko <Lotishko_sveta@mail.ru>
2015-09-02 01:06:51 +0300

    Update Main.java

--- src/main/java/com/bigcompany/managerEmployee/util/Main.java
+++ src/main/java/com/bigcompany/managerEmployee/util/Main.java
@@ -6,16 +6,6 @@ import com.bigcompany.managerEmployee.dto.Employee;
 public class Main {
 	public static void main (String[] args){
 		
-		ImplDAO dao = new ImplDAO();
-//		System.out.println(dao.readEmployByID(3
-//				));
-//		dao.updateEmployee(employee);
-		Employee employee = new Employee(" Anna", 7000);
-//		dao.updateEmployee(employee);
-//		dao.createEmployee(employee);
-		System.out.println(" Fin");
-		
-		
 	}
 
 }
